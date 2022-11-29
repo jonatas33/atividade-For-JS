81e20b232df7105587033eb7be40565a6770d8fd
Jonatas Oliveira <116115754+jonatas33@users.noreply.github.com>
2022-11-28 23:00:38 -0300

    Add files via upload

---
+++ ex01for.js
@@ -0,0 +1,18 @@
+//Exercício Sobre For
+//A instrução for cria um loop que consiste em três expressões opcionais, dentro de parênteses e separadas por ponto e vírgula, seguidas por uma declaração ou uma sequência de declarações executadas em sequência.
+
+//for ([inicialização]; [condição]; [expressão final])
+//declaração
+
+/*Usando for
+A declaração for começa declarando a variável i e inicializando-a como 0. Ela verifica se i é menor que nove, executa as duas instruções subsequentes e incrementa 1 a variável i após cada passagem pelo loop.
+
+for (var i = 0; i < 9; i++) {
+   console.log(i);
+   // more statements
+}
+*/
+
+for (let i = 0; i < 5; i++){
+    console.log('Olá Mundo! Estou aprendendo sobre For em JS', i);
+}
